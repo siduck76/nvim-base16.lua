78629097481dc796a954ec91ce6ad10293b7dd93
juan <juancloudcomputing@protonmail.com>
2021-08-22 21:22:22 +0800

    add iceberg theme
    https://terminal.sexy/#FhghxsjRHiEy4nh4tL6C4qR4hKDGoJPHibjCxsjRa3CJ6YmJwMqO6bGJkazRraDTlcTO0tTe

---
+++ lua/themes/iceberg.lua
@@ -0,0 +1,19 @@
+return {
+    base00 = "1e2132",
+    base01 = "e27878",
+    base02 = "b4be82",
+    base03 = "e2a478",
+    base04 = "84a0c6",
+    base05 = "a093c7",
+    base06 = "89b8c2",
+    base07 = "c6c8d1",
+    base08 = "6b7089",
+    base09 = "e98989",
+    base0A = "c0ca8e",
+    base0B = "e9b189",
+    base0C = "91acd1",
+    base0D = "ada0d3",
+    base0E = "95c4ce",
+    base0F = "d2d4de"
+}
+
