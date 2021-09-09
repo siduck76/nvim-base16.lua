646946d1b9a213eef807b801f4ace3f17495198d
Leo Andrade <od.leonardo01@gmail.com>
2021-09-09 20:57:55 -0300

    updating one dark colorscheme

--- lua/themes/onedark-base16.lua
+++ lua/themes/onedark-base16.lua
@@ -1,18 +1,21 @@
 return {
-    base0A = "e5c07b",
-    base04 = "565c64",
-    base07 = "c8ccd4",
-    base05 = "abb2bf",
-    base0E = "c678dd",
-    base0D = "61afef",
-    base0C = "56b6c2",
-    base0B = "98c379",
-    base02 = "3e4451",
-    base0F = "be5046",
-    base03 = "545862",
-    base08 = "e06c75",
-    base01 = "353b45",
-    base00 = "1e222a",
-    base09 = "d19a66",
-    base06 = "b6bdca"
+    base0A = "e5c07b", -- #e5c07b
+    base04 = "565c64", -- #565c64
+    base07 = "c8ccd4", -- #c8ccd4
+    base05 = "abb2bf", -- #abb2bf
+    base0E = "c678dd", -- #c678dd
+    -- base0D = "61afef", -- #61afef
+    base0D = "81a1c1", -- #81a1c1
+    base0C = "56b6c2", -- #56b6c2
+    base0B = "7ec7a2", -- #7ec7a2
+    base02 = "3e4451", -- #3e4451
+    -- base0F = "be5046", -- #be5046
+    base0F = "61afef", -- #61afef
+    base03 = "545862", -- #545862
+    -- base08 = "e06c75", -- #e06c75
+    base08 = "a67aeb", -- #a67aeb
+    base01 = "353b45", -- #353b45
+    base00 = "1e222a", -- #1e222a
+    base09 = "d19a66", -- #d19a66
+    base06 = "b6bdca" -- #b6bdca
 }
