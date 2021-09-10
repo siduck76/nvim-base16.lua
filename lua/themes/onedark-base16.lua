11ca96cbcae9edd78f77cb47dbf93f7b05214d25
Leo Andrade <od.leonardo01@gmail.com>
2021-09-09 21:34:58 -0300

    updating onedark colorscheme

--- lua/themes/onedark-base16.lua
+++ lua/themes/onedark-base16.lua
@@ -1,21 +1,24 @@
 return {
-    base0A = "e5c07b", -- #e5c07b
+    -- base0A = "e5c07b", -- #e5c07b
+    base0A = "e5c07b", -- #ebcb8b
     base04 = "565c64", -- #565c64
     base07 = "c8ccd4", -- #c8ccd4
     base05 = "abb2bf", -- #abb2bf
     base0E = "a67aeb", -- #a67aeb 
-    base0D = "61afef", -- #61afef
-    -- base0D = "81a1c1", -- #81a1c1
+    -- base0D = "61afef", -- #61afef
+    base0D = "81a1c1", -- #81a1c1
     base0C = "56b6c2", -- #56b6c2
-    base0B = "7ec7a2", -- #7ec7a2
+    base0B = "a3be8c", -- #7ec7a2 #a3be8c
     base02 = "3e4451", -- #3e4451
     -- base0F = "be5046", -- #be5046
-    base0F = "c678dd", -- #c678dd
+    -- base0F = "c678dd", -- #c678dd
+    base0F = "eceff4", -- #eceff4
     base03 = "545862", -- #545862
     -- base08 = "e06c75", -- #e06c75
-    base08 = "ca6c75", -- #ca6c75
+    base08 = "d8dee9", -- #d8dee9
     base01 = "353b45", -- #353b45
     base00 = "1e222a", -- #1e222a
-    base09 = "d19a66", -- #d19a66
+    -- base09 = "d19a66", -- #d19a66
+    base09 = "d19a66", -- #b48ead
     base06 = "b6bdca" -- #b6bdca
 }
