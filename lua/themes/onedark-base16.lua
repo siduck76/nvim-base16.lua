571c285d9ea86debbdcb1cad47ba8e9a4f60e196
Leo Andrade <od.leonardo01@gmail.com>
2021-09-09 22:29:56 -0300

    updating onedark colorscheme

--- lua/themes/onedark-base16.lua
+++ lua/themes/onedark-base16.lua
@@ -22,13 +22,13 @@ return {
     base07 = "c8ccd4", -- #c8ccd4
     base05 = "abb2bf", -- #abb2bf
     base0E = "ae90d6", -- #ae90d6
-    base0D = "61afef", -- #61afef
+    base0D = "93bfce", -- #93bfce
     base0C = "56b6c2", -- #56b6c2
     base0B = "98c379", -- #98c379
     base02 = "3e4451", -- #3e4451
-    base0F = "be5046", -- #be5046
+    base0F = "be5046", -- #ffffff
     base03 = "545862", -- #545862
-    base08 = "e06c75", -- #e06c75
+    base08 = "e06c75", -- #eceff4
     base01 = "353b45", -- #353b45
     base00 = "1e222a", -- #1e222a
     base09 = "d19a66", -- #d19a66
