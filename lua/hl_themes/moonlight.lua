a3d8e80082207629c9cc0674bfaeb4858e344804
johnsiras <jamespendon388@gmail.com>
2022-03-18 10:24:08 +0800

    Added moonlight theme

---
+++ lua/hl_themes/moonlight.lua
@@ -0,0 +1,33 @@
+return {
+   white = "#EEFFFF",
+   darker_black = "#1b1d31",
+   black = "#212337", --  nvim bg
+   black2 = "#212337",
+   one_bg = "#2b2d41", -- real bg of onedark
+   one_bg2 = "#34364a",
+   one_bg3 = "#3c3e52",
+   grey = "#494b5f",
+   grey_fg = "#535569",
+   grey_fg2 = "#5d5f73",
+   light_grey = "#65677b",
+   red = "#ff757f",
+   baby_pink = "#ff848e",
+   pink = "#ecb2f0",
+   line = "#121428", -- for lines like vertsplit
+   green = "#2df4c0",
+   vibrant_green = "#26edb9",
+   nord_blue = "#00c4ec",
+   blue = "#04d1f9",
+   yellow = "#ffc777",
+   sun = "#ffcf7f",
+   purple = "#b4a4f4",
+   dark_purple = "#9890d0",
+   teal = "#d6e7f0",
+   orange = "#f67f81",
+   cyan = "#b994f1",
+   statusline_bg = "#25273b",
+   lightbg = "#323448",
+   lightbg2 = "#2c2e42",
+   pmenu_bg = "#1b1c2b",
+   folder_bg = "#04d1f9",
+}
---
+++ lua/themes/moonlight-base16.lua
@@ -0,0 +1,18 @@
+return {
+   base00 = "212337",
+   base01 = "1B1E2B",
+   base02 = "403c64",
+   base03 = "7486d6",
+   base04 = "e4f3fa",
+   base05 = "b994f1",
+   base06 = "e4f3fa",
+   base07 = "EEFFFF",
+   base08 = "a1abe0",
+   base09 = "b4a4f4",
+   base0A = "EEFFFF",
+   base0B = "2df4c0",
+   base0C = "04d1f9",
+   base0D = "04d1f9",
+   base0E = "ffc777",
+   base0F = "ff757f",
+}
