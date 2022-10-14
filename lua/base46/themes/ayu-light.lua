7012a4eb23f833bce8db1c06f43f7d13aeb08d05
derek_lee <20820343@student.curtin.edu.au>
2022-10-14 18:13:33 +0400

    Added Ayu-Light colorscheme
    Changed base colors
    
    Changed purple color to deeper one
    
    Removed polish_hl
    
    Revert "Removed polish_hl"
    
    This reverts commit c80e398f3341b076956d59fc2f130ce189c8c015.
    
    Changed base 30 colors up to light grey
    
    Changed base16 colors
    
    Changed base colors of base 05, 06, 07
    
    Changed some TS colors
    
    Added color for comment
    
    Changed TS highlights
    
    Changed comment color
    
    testing change of base03
    
    trying to find base for comments
    
    testing for commetns
    
    Changed base02
    
    Reverted colors for base16
    
    First completion
    
    Changed color of highlight to darker
    
    Darkened color of highlight
    
    Changed highlight color to lighter
    
    Changed color of keyword to darker orange
    
    Changed color of luaTSField
    
    Swapped colors between base 0F and 0D
    
    Cleaneup code and used a lighter white color

---
+++ lua/base46/themes/ayu-light.lua
@@ -0,0 +1,67 @@
+local M = {}
+
+M.base_30 = {
+  white = "#26292f",
+  darker_black = "#f6f6f6",
+  black = "#f0f0f0", --  nvim bg
+  black2 = "#eaeaea",
+  one_bg = "#e6e6e6",
+  one_bg2 = "#dddddd", -- Highlight of context
+  one_bg3 = "#d5d5d5",
+  grey = "#c8c8c8",
+  grey_fg = "#bebebe",
+  grey_fg2 = "#a0a0a0", -- Highlight background
+  light_grey = "#989898", -- Line numbers
+  red = "#E65050",
+  baby_pink = "#ff8282",
+  pink = "#ffa5a5",
+  line = "#8A9199", -- for lines like vertsplit
+  green = "#6CBF43",
+  vibrant_green = "#94e76b",
+  blue = "#399EE6",
+  nord_blue = "#2c91d9",
+  yellow = "#E6BA7E",
+  sun = "#f3c78b",
+  purple = "#9F40FF",
+  dark_purple = "#8627e6",
+  teal = "#74c5aa",
+  orange = "#FA8D3E",
+  cyan = "#95E6CB",
+  statusline_bg = "#ebebeb",
+  lightbg = "#e2e2e2",
+  pmenu_bg = "#e9e9e9",
+  folder_bg = "#399EE6",
+}
+
+M.base_16 = {
+  base00 = "#f0f0f0",
+  base01 = "#f4f4f4",
+  base02 = "#c2c2c3",
+  base03 = "#787B80",
+  base04 = "#64676c",
+  base05 = "#5C6166",
+  base06 = "#52575c",
+  base07 = "#484d52",
+  base08 = "#F07171",
+  base09 = "#A37ACC",
+  base0A = "#399EE6",
+  base0B = "#86B300",
+  base0C = "#4CBF99",
+  base0D = "#55B4D4",
+  base0E = "#FA8D3E",
+  base0F = "#F2AE49",
+}
+
+M.polish_hl = {
+  TSTagDelimiter = { fg = M.base_30.cyan },
+  luaTSField = { fg = M.base_16.base0E },
+  TSParameter = { fg = M.base_16.base0F },
+  TSConstructor = { fg = M.base_16.base09 },
+  TSTagAttribute = { fg = M.base_30.base0F },
+}
+
+M = require("base46").override_theme(M, "ayu-light")
+
+vim.opt.bg = "light"
+
+return M
