5a7581b771fb3f56ae66e44fce168563959774fe
Sidhanth Rathod <siduck@tutanota.com>
2022-10-20 06:23:52 +0530

    improve ayu-light

--- lua/base46/themes/ayu-light.lua
+++ lua/base46/themes/ayu-light.lua
@@ -2,20 +2,20 @@ local M = {}
 
 M.base_30 = {
   white = "#26292f",
-  darker_black = "#f6f6f6",
-  black = "#f0f0f0", --  nvim bg
-  black2 = "#eaeaea",
-  one_bg = "#e6e6e6",
-  one_bg2 = "#dddddd", -- Highlight of context
-  one_bg3 = "#d5d5d5",
-  grey = "#c8c8c8",
-  grey_fg = "#bebebe",
-  grey_fg2 = "#a0a0a0", -- Highlight background
-  light_grey = "#989898", -- Line numbers
+  darker_black = "#ededed",
+  black = "#fafafa", --  nvim bg
+  black2 = "#e3e3e3",
+  one_bg = "#ebebeb",
+  one_bg2 = "#e1e1e1", -- Highlight of context
+  one_bg3 = "#d7d7d7",
+  grey = "#cdcdcd",
+  grey_fg = "#b9b9b9",
+  grey_fg2 = "#acacac", -- Highlight background
+  light_grey = "#a0a0a0", -- Line numbers
   red = "#E65050",
   baby_pink = "#ff8282",
   pink = "#ffa5a5",
-  line = "#8A9199", -- for lines like vertsplit
+  line = "#e1e1e1", -- for lines like vertsplit
   green = "#6CBF43",
   vibrant_green = "#94e76b",
   blue = "#399EE6",
@@ -27,18 +27,18 @@ M.base_30 = {
   teal = "#74c5aa",
   orange = "#FA8D3E",
   cyan = "#95E6CB",
-  statusline_bg = "#ebebeb",
-  lightbg = "#e2e2e2",
-  pmenu_bg = "#e9e9e9",
-  folder_bg = "#399EE6",
+  statusline_bg = "#f0f0f0",
+  lightbg = "#e6e6e6",
+  pmenu_bg = "#95E6CB",
+  folder_bg = "#5C6166",
 }
 
 M.base_16 = {
-  base00 = "#f0f0f0",
-  base01 = "#f4f4f4",
-  base02 = "#c2c2c3",
-  base03 = "#787B80",
-  base04 = "#64676c",
+  base00 = "#fafafa",
+  base01 = "#f0f0f0",
+  base02 = "#e6e6e6",
+  base03 = "#dcdcdc",
+  base04 = "#d2d2d2",
   base05 = "#5C6166",
   base06 = "#52575c",
   base07 = "#484d52",
@@ -54,6 +54,7 @@ M.base_16 = {
 
 M.polish_hl = {
   luaTSField = { fg = M.base_16.base0E },
+  PmenuSel = { fg = M.base_30.white, bg = M.base_30.pmenu_bg },
   ["@tag.delimiter"] = { fg = M.base_30.base0D },
   ["@parameter"] = { fg = M.base_16.base09 },
   ["@constructor"] = { fg = M.base_16.base0C },
