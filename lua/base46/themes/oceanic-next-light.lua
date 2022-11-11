2586b7fdb8788920543e1444b0321dda9a4b3e24
Sidhanth Rathod <siduck@tutanota.com>
2022-11-11 09:37:54 +0530

    Update oceanic-next-light.lua

--- lua/base46/themes/oceanic-next-light.lua
+++ lua/base46/themes/oceanic-next-light.lua
@@ -5,21 +5,21 @@ local M = {}
 
 M.base_30 = {
   white = "#26292f",
-  darker_black = "#dee4ef",
+  darker_black = "#d1d7e2",
   black = "#d8dee9", --  nvim bg
-  black2 = "#d2d8e3",
-  one_bg = "#ced4df",
-  one_bg2 = "#c5cbd6", -- Highlight of context
-  one_bg3 = "#bdc3ce",
-  grey = "#b0b6c1",
-  grey_fg = "#a6acb7",
-  grey_fg2 = "#9ca2ad", -- Highlight background
-  light_grey = "#949aa5", -- Line numbers
+  black2 = "#cbd1dc",
+  one_bg = "#cbd1dc",
+  one_bg2 = "#c2c8d3", -- Highlight of context
+  one_bg3 = "#bac0cb",
+  grey = "#adb3be",
+  grey_fg = "#a3a9b4",
+  grey_fg2 = "#999faa", -- Highlight background
+  light_grey = "#9197a2", -- Line numbers
   red = "#b40b11",
   baby_pink = "#ff8282",
   pink = "#ffa5a5",
-  line = "#c9cfda", -- for lines like vertsplit
-  green = "#869235",
+  line = "#c6ccd7", -- for lines like vertsplit
+  green = "#5b9c90",
   vibrant_green = "#9fab4e",
   nord_blue = "#708db1",
   blue = "#526f93",
@@ -30,9 +30,9 @@ M.base_30 = {
   teal = "#1abc9c",
   orange = "#f99157",
   cyan = "#6BE4E6",
-  statusline_bg = "#d4dae5",
-  lightbg = "#c7cdd8",
-  pmenu_bg = "#cdd3de",
+  statusline_bg = "#cfd5e0",
+  lightbg = "#c2c8d3",
+  pmenu_bg = "#5b9c90",
   folder_bg = "#526f93",
 }
 
@@ -42,7 +42,7 @@ M.base_16 = {
   base02 = "#c0c5ce",
   base03 = "#a7adba",
   base04 = "#65737e",
-  base05 = "#4f5b66",
+  base05 = "#343d46",
   base06 = "#343d46",
   base07 = "#1b2b34",
   base08 = "#b40b11",
@@ -58,5 +58,14 @@ M.base_16 = {
 vim.opt.bg = "light"
 
 M = require("base46").override_theme(M, "oceanic-next-light")
+
+M.polish_hl = {
+  St_NTerminalMode = { fg = M.base_30.white },
+  St_VisualMode = { bg = M.base_30.teal },
+  St_VisualModeSep = { fg = M.base_30.teal },
+
+  ["@field"] = { fg = M.base_16.base0C },
+  Constant = { fg = M.base_16.base09 },
+}
 
 return M
