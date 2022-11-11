7a62d570ead43365992365eb6856d9fb8030b28a
Sidhanth Rathod <siduck@tutanota.com>
2022-11-11 06:23:29 +0530

    Improve monochrome theme

--- lua/base46/themes/monochrome.lua
+++ lua/base46/themes/monochrome.lua
@@ -1,55 +1,62 @@
+-- credit to original theme for existing : https://github.com/kdheepak/monochrome.nvim
+-- NOTE: This is a modified version of it
+
 local M = {}
 
 M.base_30 = {
   white = "#D8DEE9",
-  darker_black = "#0a0a0a",
-  black = "#0a0a0a", --  nvim bg
-  black2 = "#161616",
-  one_bg = "#1a1a1a",
-  one_bg2 = "#232323",
-  one_bg3 = "#2b2b2b",
-  grey = "#383838",
-  grey_fg = "#424242",
-  grey_fg2 = "#4c4c4c",
-  light_grey = "#545454",
-  red = "#dc322f",
-  baby_pink = "#de878f",
+  darker_black = "#1a1a1a",
+  black = "#101010", --  nvim bg
+  black2 = "#202020",
+  one_bg = "#242424",
+  one_bg2 = "#2e2e2e",
+  one_bg3 = "#383838",
+  grey = "#424242",
+  grey_fg = "#4c4c4c",
+  grey_fg2 = "#606060",
+  light_grey = "#677777",
+  red = "#ec8989",
+  baby_pink = "#eca8a8",
   pink = "#da838b",
-  line = "#1a1a1a", -- for lines like vertsplit
-  green = "#719e07",
+  line = "#2e2e2e", -- for lines like vertsplit
+  green = "#c9d36a",
   vibrant_green = "#eff6ab",
-  blue = "#268bd2",
-  nord_blue = "#8abae1",
-  yellow = "#b58900",
-  sun = "#e1c181",
+  blue = "#8abae1",
+  nord_blue = "#a5c6e1",
+  yellow = "#ffe6b5",
+  sun = "#eff6ab",
   purple = "#e1bee9",
   dark_purple = "#db9fe9",
   teal = "#6484a4",
   orange = "#efb6a0",
   cyan = "#9aafe6",
-  statusline_bg = "#333945",
-  lightbg = "#3f4551",
-  pmenu_bg = "#A3BE8C",
+  statusline_bg = "#1e1e1e",
+  lightbg = "#2e2e2e",
+  pmenu_bg = "#859ba2",
   folder_bg = "#7797b7",
 }
 
 M.base_16 = {
   base00 = "#101010",
-  base01 = "#1a1a1a",
-  base02 = "#434C5E",
-  base03 = "#4C566A",
-  base04 = "#566074",
+  base01 = "#1f1f1f",
+  base02 = "#2e2e2e",
+  base03 = "#383838",
+  base04 = "#424242",
   base05 = "#bfc5d0",
   base06 = "#c7cdd8",
   base07 = "#ced4df",
   base08 = "#eee8d5",
-  base09 = "#eee8d5",
-  base0A = "#586e75",
-  base0B = "#6c8289",
-  base0C = "#eee8d5",
+  base09 = "#B8B7B1",
+  base0A = "#859ba2",
+  base0B = "#7b9198",
+  base0C = "#DFDFDA",
   base0D = "#ced4df",
-  base0E = "#eee8d5",
+  base0E = "#DAD4C3",
   base0F = "#ced4df",
+}
+
+M.polish_hl = {
+  ["@punctuation.bracket"] = { fg = M.base_30.red },
 }
 
 vim.opt.bg = "dark"
