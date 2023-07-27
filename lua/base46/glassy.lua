6eee5fa10ec7b9cabc483d549b793b345db43fbd
LongdeLao08 <135625853+LongdeLao08@users.noreply.github.com>
2023-07-27 19:50:40 +0800

    Update glassy.lua
    Statusline should be now transparent when toggling transparency mode

--- lua/base46/glassy.lua
+++ lua/base46/glassy.lua
@@ -10,26 +10,31 @@ local M = {
     fg = colors.black,
     bg = colors.blue,
   },
+
+  -- for hl groups which need bg = "NONE" only!
+  local hl_groups = {
+    "NormalFloat",
+    "Normal",
+    "Folded",
+    "NvimTreeNormal",
+    "NvimTreeNormalNC",
+    "NvimTreeCursorLine",
+    "TelescopeNormal",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "TelescopePromptNormal",
+    "TelescopePromptPrefix",
+    "CursorLine",
+    "Pmenu",
+    "CmpPmenu",
+  },
+
+  StatusLine = {
+    bg = "NONE",
+  },
 }
 
--- for hl groups which need bg = "NONE" only!
-local hl_groups = {
-  "NormalFloat",
-  "Normal",
-  "Folded",
-  "NvimTreeNormal",
-  "NvimTreeNormalNC",
-  "NvimTreeCursorLine",
-  "TelescopeNormal",
-  "TelescopePrompt",
-  "TelescopeResults",
-  "TelescopePromptNormal",
-  "TelescopePromptPrefix",
-  "CursorLine",
-  "Pmenu",
-  "CmpPmenu",
-}
-
+-- Set "bg = 'NONE'" for all the specified highlight groups.
 for _, groups in ipairs(hl_groups) do
   M[groups] = {
     bg = "NONE",
