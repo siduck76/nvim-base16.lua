3038abfe5a0d3292cc1d512a4b3dac2b5b553c73
siduck <siduck@tutanota.com>
2024-08-08 05:24:14 +0530

    add missing transparency support for base46.transparancy in nvconfig #324

--- lua/base46/integrations/statusline/default.lua
+++ lua/base46/integrations/statusline/default.lua
@@ -1,9 +1,9 @@
-local config = require("nvconfig").ui
+local config = require "nvconfig"
 local get_theme_tb = require("base46").get_theme_tb
 local colors = get_theme_tb "base_30"
 local generate_color = require("base46.colors").change_hex_lightness
 
-local statusline_bg = config.transparency and "NONE" or colors.statusline_bg
+local statusline_bg = (config.ui.transparency or config.base46.transparency) and "NONE" or colors.statusline_bg
 local light_grey = generate_color(colors.light_grey, 8)
 
 local M = {
