9b81403ef6e29a70bffb7990a0f3dbe5f6095047
siduck <siduck@tutanota.com>
2024-09-06 19:06:14 +0530

    fix: custom theme crashing telescope theme switcher

--- lua/base46/color_vars.lua
+++ lua/base46/color_vars.lua
@@ -1,8 +1,10 @@
 local opts = require("nvconfig").base46
 local str = ""
-local colors = require("base46.themes." .. opts.theme).base_30
 
-for name, hex in pairs(colors) do
+local present1, default_theme = pcall(require, "base46.themes." .. opts.theme)
+local colors = (present1 and default_theme) or require("themes." .. opts.theme)
+
+for name, hex in pairs(colors.base_30) do
   str = str .. name .. "='" .. hex
   str = str .. "',"
 end
