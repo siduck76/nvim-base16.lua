64444823899f0322b70f2502fa14dc884901e833
Hackcraft_ <hackcraft_@hotmail.com>
2024-10-09 10:52:24 -0400

    Update markview.lua

--- lua/base46/integrations/markview.lua
+++ lua/base46/integrations/markview.lua
@@ -1,10 +1,10 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 return {
-  ["@markup.heading.1.markdown"] = { bg = colors.red1, fg = colors.red2 },
-  ["@markup.heading.2.markdown"] = { bg = colors.orange1, fg = colors.orange2 },
-  ["@markup.heading.3.markdown"] = { bg = colors.yellow1, fg = colors.yellow2 },
-  ["@markup.heading.4.markdown"] = { bg = colors.green1, fg = colors.green2 },
-  ["@markup.heading.5.markdown"] = { bg = colors.blue1, fg = colors.blue2 },
-  ["@markup.heading.6.markdown"] = { bg = colors.purple1, fg = colors.purple2 },
+  ["@markup.heading.1.markdown"] = { fg = colors.red },
+  ["@markup.heading.2.markdown"] = { fg = colors.orange },
+  ["@markup.heading.3.markdown"] = { fg = colors.yellow },
+  ["@markup.heading.4.markdown"] = { fg = colors.green },
+  ["@markup.heading.5.markdown"] = { fg = colors.blue },
+  ["@markup.heading.6.markdown"] = { fg = colors.purple },
 }
