f006ffdd0e4c0a8b1a639713f5208bc282c60058
Daniel Odrinski <m4tr1xc0d3r@protonmail.com>
2024-11-01 01:16:27 +0100

    feat: diffview.nvim integration (#359)
    This integration fixes the colourscheme for diffview.nvim
    and matches it to that of GitHub code diff view.
    
    Optimized for and tested with dark mode.

---
+++ lua/base46/integrations/diffview.lua
@@ -0,0 +1,13 @@
+local colors = require("base46").get_theme_tb "base_30"
+local mix_col = require("base46.colors").mix
+
+local highligths = {
+  DiffviewDiffAdd = { bg = mix_col(colors.green, colors.black, 85) },
+  DiffviewDiffText = { bg = mix_col(colors.green, colors.black, 70) },
+  DiffviewDiffChange = { bg = mix_col(colors.green, colors.black, 85) },
+  DiffviewDiffDelete = { bg = mix_col(colors.red, colors.black, 70) },
+  DiffviewDiffDeleteDim = { bg = mix_col(colors.red, colors.black, 85) },
+  DiffviewDiffAddAsDelete = { bg = mix_col(colors.red, colors.black, 85) },
+}
+
+return highligths
