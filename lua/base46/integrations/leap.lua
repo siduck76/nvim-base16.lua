806429e1ab706a30ac57e671d028555445850186
GCBallesteros <dev+github@maxwellrules.com>
2024-11-22 10:26:34 +0100

    feat: add leap.nvim integration

---
+++ lua/base46/integrations/leap.lua
@@ -0,0 +1,7 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+return {
+  LeapBackdrop = { fg = colors.grey_fg },
+  LeapLabel = { fg = colors.yellow, bold = true },
+  LeapMatch = { fg = colors.yellow, bold = true },
+}
