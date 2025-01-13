20bd6a59fbd2a0f70bc4f56902952ab7a4254604
siduck <siduck@tutanota.com>
2025-01-13 20:21:25 +0530

    fix(lspsaga integration): dont color bg if transparency enabled

--- lua/base46/integrations/lspsaga.lua
+++ lua/base46/integrations/lspsaga.lua
@@ -1,5 +1,11 @@
 local colors = require("base46").get_theme_tb "base_30"
 
+local opts = require("nvconfig").base46
+
+if opts.transparency then
+  return {}
+end
+
 return {
   SagaBorder = {
     bg = colors.darker_black,
