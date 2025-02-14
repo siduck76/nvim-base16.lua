fd0afaf258a32d6734c14e6ad404cf23d30c8dd0
siduck <siduck@tutanota.com>
2025-02-14 06:33:59 +0530

    improve default diff highlightgroups by colored bg

--- lua/base46/integrations/diffview.lua
+++ lua/base46/integrations/diffview.lua
@@ -2,10 +2,10 @@ local colors = require("base46").get_theme_tb "base_30"
 local mix_col = require("base46.colors").mix
 
 local highligths = {
-  DiffviewDiffAdd = { bg = mix_col(colors.green, colors.black, 85) },
-  DiffviewDiffText = { bg = mix_col(colors.green, colors.black, 70) },
-  DiffviewDiffChange = { bg = mix_col(colors.green, colors.black, 85) },
-  DiffviewDiffDelete = { bg = mix_col(colors.red, colors.black, 70) },
+  -- DiffviewDiffAdd = { bg = mix_col(colors.green, colors.black, 85) },
+  -- DiffviewDiffText = { bg = mix_col(colors.green, colors.black, 70) },
+  -- DiffviewDiffChange = { bg = mix_col(colors.green, colors.black, 85) },
+  -- DiffviewDiffDelete = { bg = mix_col(colors.red, colors.black, 70) },
   DiffviewDiffDeleteDim = { bg = mix_col(colors.red, colors.black, 85) },
   DiffviewDiffAddAsDelete = { bg = mix_col(colors.red, colors.black, 85) },
 }
--- lua/base46/integrations/git.lua
+++ lua/base46/integrations/git.lua
@@ -1,5 +1,6 @@
 local theme = require("base46").get_theme_tb "base_16"
 local colors = require("base46").get_theme_tb "base_30"
+local mix = require("base46.colors").mix
 
 return {
 
@@ -12,30 +13,37 @@ return {
   },
 
   DiffAdd = {
-    fg = colors.blue,
+    bg = mix(colors.green, colors.black, 90),
+    fg = colors.green,
   },
 
   DiffAdded = {
+    bg = mix(colors.green, colors.black, 90),
     fg = colors.green,
   },
 
   DiffChange = {
+    bg = mix(colors.light_grey, colors.black, 90),
     fg = colors.light_grey,
   },
 
   DiffChangeDelete = {
+    bg = mix(colors.red, colors.black, 90),
     fg = colors.red,
   },
 
   DiffModified = {
+    bg = mix(colors.orange, colors.black, 90),
     fg = colors.orange,
   },
 
   DiffDelete = {
+    bg = mix(colors.red, colors.black, 90),
     fg = colors.red,
   },
 
   DiffRemoved = {
+    bg = mix(colors.red, colors.black, 90),
     fg = colors.red,
   },
 
